2e0dd84294b75fcc05b9d1e1479df3b8e16b2dd2
JoaoPLF <joaoplfonseca@hotmail.com>
2024-09-02 18:27:17 -0400

    Adiciona api gateway ao projeto

--- run-dev.sh
+++ run-dev.sh
@@ -1,1 +1,1 @@
-(cd produtos && ./gradlew build --no-daemon) && (cd usuario && ./gradlew build --no-daemon) && docker-compose -f docker-compose.dev.yml up -d
+(cd produtos && ./gradlew build --no-daemon) && (cd usuario && ./gradlew build --no-daemon) && (cd api-gateway && ./gradlew build --no-daemon) && docker-compose -f docker-compose.dev.yml up -d
